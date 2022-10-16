e064668f89a92bb4180f1686f6a35759f28cf50e
AW666 <45335490+AW666@users.noreply.github.com>
2022-10-16 16:27:07 -0400

    Add files via upload

---
+++ Data Cleansing & Transformation/DimCalendar.sql
@@ -0,0 +1,25 @@
+-- Cleansed DIM DateTable
+SELECT 
+  [DateKey], 
+  [FullDateAlternateKey] AS Date, 
+  --,[DayNumberOfWeek]
+  [EnglishDayNameOfWeek] AS Day, 
+  --,[SpanishDayNameOfWeek]
+  --,[FrenchDayNameOfWeek]
+  --,[DayNumberOfMonth]
+  --,[DayNumberOfYear]
+  [WeekNumberOfYear] AS WeekNr, 
+  [EnglishMonthName] AS Month,
+  LEFT([EnglishMonthName], 3) AS MonthAcronym,
+  --,[SpanishMonthName]
+  --,[FrenchMonthName]
+  [MonthNumberOfYear] AS MonthNo, 
+  [CalendarQuarter] AS Quarter, 
+  [CalendarYear] AS Year --,[CalendarSemester]
+  --,[FiscalQuarter]
+  --,[FiscalYear]
+  --,[FiscalSemester]
+FROM 
+  [AdventureWorksDW2019].[dbo].[DimDate] 
+WHERE 
+  CalendarYear >= 2020
